dbcb6162ce83a7ca116b774049ef5482dbbaa71b
Yasmin Mehattar <yasminmehattar@gmail.com>
2025-08-14 23:25:33 +0530

    commit

--- my-reactapp/src/btn.jsx
+++ my-reactapp/src/btn.jsx
@@ -1,17 +1,17 @@
 function Buton(){
-    const handleClick= () => console.log("OUCH")
+    // const handleClick= () => console.log("OUCH")
     // const handleClick2=(name)=>console.log(`${name} stop clicking me`)
-    // let count=0;
-    // const handleClick2=(name)=>{
-    // if (count<3){
-    // count++;
-    // console.log(`${name}clicked me${count}times`)}
-    // else{
-    //     console.log(`${name}stop clicking me`)
-    // }}
-    const handleClick2= (e)=>e.target.textContent="OUCH";
+    let count=0;
+    const handleClick2=(name)=>{
+    if (count<3){
+    count++;
+    console.log(`${name}clicked me${count}times`)}
+    else{
+        console.log(`${name}stop clicking me`)
+    }}
+    // const handleClick2= (e)=>e.target.textContent="OUCH";
     return(
-        <button onDoubleClick={(e)=>handleClick2 (e)}>Click</button>
+        <button onClick={()=>handleClick2 ("yasmin")}>Click</button>
 
     )
 }
