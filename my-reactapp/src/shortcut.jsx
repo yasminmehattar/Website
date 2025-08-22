c847868b8b8e74c626709b9045d6a986834a6c49
Yasmin Mehattar <yasminmehattar@gmail.com>
2025-08-22 22:53:39 +0530

    commit

--- my-reactapp/src/shortcut.jsx
+++ my-reactapp/src/shortcut.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import style from "styled-components"
+const Message=(props)=>{
+  const {text,bgcolor}=props
+  return <div>
+    this is my first message
+    {text}
 
-function shortcut() {
-  return (
-    <container>Shortcut</container> 
-  )
+    </div>
 }
 
-export default shortcut
+
+const App=()=>{
+  return <div>
+    <Message text="this is my first prop " bgcolor="red"/>
+    </div>
+ };
+
+ function App(){
+  return(
+    <div>
+      <Message1 text="this is my second prop" bgcolor="green"/>
+      </div>
+  )
+ }
+ function Message1(props){
+  const {text,bgcolor}=props
+  return(
+    <div>
+      This is my first message
+      {text}
+      </div>
+
+  )
+ }
+
+ 
+export default App
+
